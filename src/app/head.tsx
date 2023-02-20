f50e2e902ddea129be4a7ba8c0dcf79479a8da33
Paul Magnus <pauljmagnus@gmail.com>
2023-02-19 23:47:28 -0600

    Create NextJS app

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Paul J Magnus - Software Developer</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Paul J. Magnus software developer at Epic" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
